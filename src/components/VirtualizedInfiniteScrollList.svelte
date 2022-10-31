af6905c0d8cae4dd5af1f81789c44448f65e8b26
Pariring <parangee9706@gmail.com>
2022-10-31 18:24:16 +0900

    feat: listen for scroll event

--- src/components/VirtualizedInfiniteScrollList.svelte
+++ src/components/VirtualizedInfiniteScrollList.svelte
@@ -1,1 +1,38 @@
-<div>Test</div>
+<script lang="ts">
+  export let scroller: HTMLElement | undefined = undefined;
+
+  let isWindow = false;
+
+  let container: HTMLDivElement;
+
+  export { isWindow as window };
+
+  $: scrollElement = isWindow ? document.documentElement : scroller;
+
+  $: listenerSubscription = {
+    subscribe: () => {
+      // eslint-disable-next-line @typescript-eslint/no-empty-function
+      if (!scrollElement) return () => {};
+
+      const elementToListen = isWindow ? window : scrollElement;
+
+      const listener = () => {
+        console.log(
+          `${scrollElement.scrollTop} - ${
+            scrollElement.scrollTop + scrollElement.offsetHeight
+          }`
+        );
+      };
+
+      elementToListen.addEventListener('scroll', listener);
+
+      return () => {
+        elementToListen.removeEventListener('scroll', listener);
+      };
+    },
+  };
+
+  $listenerSubscription;
+</script>
+
+<div bind:this={container}>content</div>
