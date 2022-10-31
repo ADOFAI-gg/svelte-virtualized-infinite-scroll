d692b98eef14084e50eabfe90522e0c84e8e2a93
Pariring <parangee9706@gmail.com>
2022-10-31 20:19:10 +0900

    feat: render items
    TODO: add spacing support

--- src/components/VirtualizedInfiniteScrollList.svelte
+++ src/components/VirtualizedInfiniteScrollList.svelte
@@ -1,44 +1,95 @@
 <script lang="ts">
+  // eslint-disable-next-line no-undef
+  type T = $$Generic;
+
+  export let itemHeight: number;
+
+  // export let itemSpacing = 0;
+
+  export let items: T[];
+
   export let scroller: HTMLElement | undefined = undefined;
 
   let isWindow = false;
 
   let container: HTMLDivElement;
+  let contentOffset = 0;
+
+  export let startIndex = 0;
+  export let endIndex = 0;
 
   export { isWindow as window };
 
   $: scrollElement = isWindow ? document.documentElement : scroller;
 
+  $: onScroll = async () => {
+    if (!container || !scrollElement) return;
+
+    const from = Math.max(Math.floor(scrollElement.scrollTop / itemHeight), 0);
+
+    const to = Math.max(
+      Math.ceil(
+        (scrollElement.scrollTop + scrollElement.offsetHeight) / itemHeight
+      ),
+      items.length
+    );
+
+    if (startIndex !== from) startIndex = from;
+    if (endIndex !== to) endIndex = to;
+
+    const offset = itemHeight * startIndex;
+
+    if (contentOffset !== offset) contentOffset = offset;
+  };
+
   $: listenerSubscription = {
     subscribe: () => {
       const elementToListen = isWindow ? window : scrollElement;
 
-      // eslint-disable-next-line @typescript-eslint/no-empty-function
-      if (!elementToListen) return () => {};
+      if (!elementToListen)
+        // eslint-disable-next-line @typescript-eslint/no-empty-function
+        return () => {};
 
-      const listener = () => {
-        console.log(
-          `${scrollElement.scrollTop} - ${
-            scrollElement.scrollTop + scrollElement.offsetHeight
-          }`
-        );
-      };
-
-      elementToListen.addEventListener('scroll', listener);
+      elementToListen.addEventListener('scroll', onScroll);
 
       return () => {
-        elementToListen.removeEventListener('scroll', listener);
+        elementToListen.removeEventListener('scroll', onScroll);
       };
     },
   };
 
+  $: {
+    if (items.length) {
+      onScroll();
+    }
+  }
+
+  $: toRender = items.slice(startIndex, endIndex);
+
   $listenerSubscription;
 </script>
 
-<div>
-  <!-- upper padding -->
-  <div />
-  <div bind:this={container}>content</div>
-  <!-- lower padding -->
-  <div />
+<div
+  bind:this={container}
+  class="virtualized-list-container"
+  style="height: {itemHeight * items.length}px;"
+>
+  <div class="virtualized-list-content" style="margin-top: {contentOffset}px;">
+    {#each toRender as item}
+      <div style="height: {itemHeight}px;">
+        <slot {item} />
+      </div>
+    {/each}
+  </div>
 </div>
+
+<style>
+  .virtualized-list-container {
+    position: relative;
+  }
+
+  .virtualized-list-content {
+    position: absolute;
+    width: 100%;
+  }
+</style>
--- src/stories/DefaultVirtualScrollList.svelte
+++ src/stories/DefaultVirtualScrollList.svelte
@@ -1,19 +1,51 @@
 <script lang="ts">
+  import { onMount } from 'svelte';
+
   import VirtualizedInfiniteScrollList from '../components/VirtualizedInfiniteScrollList.svelte';
 
   let container: HTMLDivElement;
+
+  let items: number[] = [];
+
+  const addItems = () => {
+    items = [
+      ...items,
+      ...new Array(100).fill(undefined).map((x, i) => items.length + i),
+    ];
+  };
+
+  onMount(() => {
+    addItems();
+  });
 </script>
 
 <h1>Virtualized Infinite Scroll List</h1>
 
+<button on:click={addItems}>Add 100 items to bottom</button>
+
 <div bind:this={container} class="container">
-  <VirtualizedInfiniteScrollList scroller={container} />
+  <VirtualizedInfiniteScrollList
+    itemHeight={128}
+    {items}
+    scroller={container}
+    let:item
+  >
+    <div class="item">
+      {item}
+    </div>
+  </VirtualizedInfiniteScrollList>
 </div>
 
 <style>
   .container {
-    height: 360px;
+    height: 540px;
     border: 1px solid rgba(0, 0, 0, 0.2);
     overflow-y: scroll;
+    margin-top: 12px;
+  }
+
+  .container :global(.item) {
+    height: 100%;
+    border: 1px solid #f00;
   }
 </style>
