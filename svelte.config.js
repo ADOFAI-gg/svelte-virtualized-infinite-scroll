5c38604a8ebbedd0ab6da416e2aadd595a8fe4f9
파링 <paring@pikokr.dev>
2023-01-28 21:27:17 +0900

    feat: setup css modules

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,12 @@
-import sveltePreprocess from 'svelte-preprocess';
+import { cssModules, linearPreprocess } from 'svelte-preprocess-cssmodules';
+import preprocess from 'svelte-preprocess';
 
 export default {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
-  preprocess: sveltePreprocess(),
+  preprocess: linearPreprocess([
+    preprocess.typescript(),
+    preprocess.scss(),
+    cssModules(),
+  ]),
 };
