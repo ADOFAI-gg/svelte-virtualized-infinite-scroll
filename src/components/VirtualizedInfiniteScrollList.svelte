c92a5bfa8b723a3cf8c7ae258ff059b2264fe482
paring <paring@pikokr.dev>
2023-05-11 20:48:24 +0900

    refactor: make scroll list component simpler

--- src/components/VirtualizedInfiniteScrollList.svelte
+++ src/components/VirtualizedInfiniteScrollList.svelte
@@ -44,33 +44,21 @@
   export { originalItems as items };
 </script>
 
-{#if items.length}
-  <div
-    style="height: {$virtualizer.getTotalSize()}px"
-    class="virtual-container"
-  >
-    <!-- <div
+<div style="height: {$virtualizer.getTotalSize()}px" class="virtual-container">
+  <!-- <div
       style="transform: translateY({items?.[0].start ?? 0}px);"
       class="content"
     > -->
-    {#each items as item}
-      <VirtualizedScrollListItem {item}>
-        <slot item={originalItems[item.index]} />
-      </VirtualizedScrollListItem>
-    {/each}
-    <!-- </div> -->
-  </div>
-{/if}
+  {#each items as item}
+    <VirtualizedScrollListItem {item}>
+      <slot item={originalItems[item.index]} />
+    </VirtualizedScrollListItem>
+  {/each}
+  <!-- </div> -->
+</div>
 
-<style module lang="scss">
+<style lang="scss">
   .virtual-container {
     position: relative;
-
-    .content {
-      position: absolute;
-      top: 0;
-      left: 0;
-      width: 100%;
-    }
   }
 </style>
